324186f32b8e85404f12264f2e3de5b83517cd6a
lucasoli <lksfx@hotmail.com>
2017-02-08 17:13:11 -0200

    Primeiro commit do projeto Unity VirusByte

---
+++ Assets/Scripts/Camera/FixedWidth.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedWidth : MonoBehaviour {
+    readonly float fixedWidth = 480;
+	// Use this for initialization
+	void Start () {
+        var camera = GetComponent<Camera>();
+        // Exemplo, no caso de uma resolução de 720x1280 a proporção seria de
+        // 1.78 pixel de altura para cada pixel de largura 
+        var ratio = (float)Screen.height / (float)Screen.width;
+        var screenHeight = fixedWidth * ratio;
+        var size = screenHeight / 160;
+        camera.orthographicSize = size;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
