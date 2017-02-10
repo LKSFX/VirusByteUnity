d3628ef1ca664bad98e5456a9e22ca8aec603f7b
lucasoli <lksfx@hotmail.com>
2017-02-10 16:00:20 -0200

    Classe ItemGrabber criada, segunda tentativa de commit.

---
+++ Assets/GameObjects/Items/Scripts/ItemGrabber.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemGrabber : Grabber {
+
+    private SpriteRenderer _sprRender;
+
+    protected override void Start() {
+        base.Start();
+        _sprRender = GetComponent<SpriteRenderer>();
+    }
+
+    protected override void Update() {
+        base.Update();
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            print("Pressed Space key");
+            StartCoroutine(Fade());
+        }
+    }
+
+    public override void onGrabHalfTargetScale() {
+        base.onGrabHalfTargetScale();
+        if (_sprRender != null) {
+            _sprRender.sortingLayerName = "LayerItems";
+        }
+    }
+
+    public override void onDropHalfTargetScale() {
+        base.onDropHalfTargetScale();
+        if (_sprRender) {
+            _sprRender.sortingLayerName = "LayerSprites0";
+        }
+    }
+
+    IEnumerator Fade() {
+        var c = _sprRender.color;
+        var f = c.a;
+        while (f >= 0) {
+            f -= .1f;
+            c.a = f;
+            _sprRender.color = c;
+            yield return new WaitForSeconds(0.3f);
+        }
+
+        print("FadeEnd: " + _sprRender.color);
+    }
+}
---
+++ Assets/SandBox/Scripts/TimeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeController : MonoBehaviour {
+
+    public void switchTimeScale() {
+        if (Time.timeScale == 1) {
+            Time.timeScale = 0;
+            GetComponentInChildren<UnityEngine.UI.Text>().text = "Play";
+        }
+        else {
+            Time.timeScale = 1;
+            GetComponentInChildren<UnityEngine.UI.Text>().text = "Stop";
+        }
+    }
+
+}
