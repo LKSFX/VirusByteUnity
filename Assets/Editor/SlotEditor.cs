cdf0601f9a0764925b87968e238cbda10bdec0e1
lucasoli <lksfx@hotmail.com>
2017-02-23 11:15:40 -0300

    Classe SlotEditor criada.

---
+++ Assets/Editor/SlotEditor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Slot))]
+public class SlotEditor : Editor {
+    SerializedProperty startWidthItem;
+
+    public override void OnInspectorGUI() {
+        var slot = target as Slot;
+        slot.startWidthItem = GUILayout.Toggle(slot.startWidthItem, "StartWithItem");
+        if (slot.startWidthItem) {
+            slot.type = (Item.ItemType)EditorGUILayout.EnumPopup("Type: ", slot.type);
+            //EditorGUILayout.LabelField("A quantidade de itens que o slot terá ao carregar a fase.");
+            slot.quantity = EditorGUILayout.IntField("Quantity: ", slot.quantity);
+        }
+    }
+
+    private void OnEnable() {
+        Debug.Log(target.name + " enabled");
+
+    }
+}
