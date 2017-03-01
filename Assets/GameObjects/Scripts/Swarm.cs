29cef47c5596bd9a876e3c3569eda4e2d0ae53f8
lucasoli <lksfx@hotmail.com>
2017-03-01 20:28:25 -0300

    Classe Swarm agora permite definir o alpha(transparencia) dos spawners.

--- Assets/GameObjects/Scripts/Swarm.cs
+++ Assets/GameObjects/Scripts/Swarm.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class Swarm : MonoBehaviour, ISpawner {
 
     public float swarmSpeedFactor = 1;
+    public float alpha = 1;
 
     private bool completed;
 
     public void spawn() {
+        if (!Application.isPlaying) return; 
         if (completed == true) return;
         completed = true;
         // check for parent
@@ -27,5 +30,40 @@ public class Swarm : MonoBehaviour, ISpawner {
                 spawner.spawn();
         }
         Destroy(gameObject); // absoleto após spawning
+    }
+
+    public void changeAlpha(Swarm swarm, float alpha) {
+        if (swarm == null) return; // não aceita valor NULO
+        SpriteRenderer sprite;
+        foreach (Transform child in swarm.transform) {
+            sprite = child.GetComponent<SpriteRenderer>();
+            if (sprite != null) {
+                Color c = sprite.color;
+                c.a = alpha;
+                sprite.color = c;
+            }
+            else if (child.GetComponent<Swarm>() != null) {
+                changeAlpha(child.GetComponent<Swarm>(), alpha);
+            }
+        }
+    }
+
+    private void OnValidate() {
+        alpha = Mathf.Clamp01(alpha);
+        changeAlpha(this, alpha);
+        //print("validate calling " + alpha);
+    }
+
+    private void OnEnable() {
+        Swarm deltaSwarm = null;
+        Transform nextParent = transform;
+        while (nextParent != null) {
+            if (nextParent.GetComponent<Swarm>() != null) {
+                deltaSwarm = nextParent.GetComponent<Swarm>();
+            }
+            nextParent = nextParent.parent;
+        }
+
+        changeAlpha(deltaSwarm, deltaSwarm.alpha);
     }
 }
